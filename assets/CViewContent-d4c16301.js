ba9d3fa9f0fddbe322eefecb6dbeae26c2bb93b5
Harald Amon <harald.amon@gmail.com>
2023-07-17 23:03:50 +0200

    Update website

--- assets/CViewContent-d4c16301.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as s,a as t,r as n}from"./main-33e76348.js";const c={},r={class:"flex flex-column gap-3"};function a(e,l){return s(),t("div",r,[n(e.$slots,"default")])}const f=o(c,[["render",a]]);export{f as C};
--- assets/band_1-8acba709.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/band_1-0e48643b.jpg";export{s as i};
