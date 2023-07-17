8e1b0d9505b5bebaf24be0f9fb675242faa9a9e6
Harald Amon <harald.amon@gmail.com>
2023-07-17 16:23:38 +0200

    Update website

--- assets/CImg-a55150e8.js
+++
@@ -1,1 +0,0 @@
-import{d as c,o as e,a,n,_}from"./main-97ae5e39.js";const o=["alt","src"],r=c({__name:"CImg",props:{alt:String,src:String,class:String},setup(t){const s=t;return(l,p)=>(e(),a("img",{alt:s.alt,src:s.src,class:n(s.class)},null,10,o))}});const m=_(r,[["__scopeId","data-v-fc8fdd44"]]);export{m as C};
