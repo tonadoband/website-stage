ae42d35e6b49cdefa07b95fb26dfc211e6bbe3e5
haramon <78597170+haramon@users.noreply.github.com>
2023-11-28 20:37:22 +0000

    version 1.8.0-rc.0

---
+++ assets/CImg-3094369a.js
@@ -0,0 +1,1 @@
+import{d as c,f as t,o,b as n,x as _,_ as r}from"./main-33319012.js";const l=c({__name:"CImg",props:{alt:String,src:String,class:String},setup(e){const s=e;return(p,m)=>{const a=t("Image");return o(),n(a,{src:s.src,alt:s.alt,class:_(s.class),preview:""},null,8,["src","alt","class"])}}});const i=r(l,[["__scopeId","data-v-c3e3a0d7"]]);export{i as C};
